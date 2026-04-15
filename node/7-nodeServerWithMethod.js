c357cc6957688bc3efb9d179139d21324239d28b
vikash kumar <vikashkumar24041997@gmail.com>
2026-04-16 00:34:02 +0530

    reading and writting file

--- node/7-nodeServerWithMethod.js
+++ node/7-nodeServerWithMethod.js
@@ -1,7 +1,12 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  if (req.url === 'user' && req.method === 'GET') {
+  if (req.url === '/user' && req.method === 'GET') {
     res.end('Get user');
   }
+  if (req.url === '/user' && req.method === 'POST') {
+    res.end('Create User');
+  }
 });
+
+server.listen(3000);
---
+++ node/build-in-module/1-fileSystem.js
@@ -0,0 +1,42 @@
+/*
+🔹 1. What is fs module?
+Built-in Node.js module (no installation required)
+Used to interact with the file system
+Works with:
+Files 📄
+Directories 📁
+*/
+
+const fs = require('fs');
+
+//Synchronous(Blocking):- stop executing until opration compleated
+
+const data = fs.readFileSync('file.txt', 'utf-8');
+console.log(data);
+
+//Asychronous(callback-bassed)
+fs.readFile('data.txt', 'utf-8', (err, data) => {
+  if (err) return console.error(err);
+  console.log(data);
+});
+
+//Promise based(fs/promises):-morden and clean
+async function readFile() {
+  try {
+    const data = await fs.readFile('file.txt', 'utf-8');
+    console.log(data);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+//write file
+/*
+🔹 1. What is writeFile?
+👉 writeFile is used to:
+Create a new file ✅
+OR overwrite an existing file ❗
+📌 If file exists → content will be replaced
+📌 If file does NOT exist → new file will be created
+*/
+fs.writeFile('data.txt', 'Hello vikash', (err) => {});
